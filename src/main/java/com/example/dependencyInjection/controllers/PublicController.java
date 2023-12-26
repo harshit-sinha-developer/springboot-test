4652c5092327aef5ff91d320cdaa819e7dcadd02
Rashmi Kumari <rashmi25346@gmail.com>
2023-12-26 08:28:59 -0500

    Updates

--- src/main/java/com/example/dependencyInjection/controllers/PublicController.java
+++ src/main/java/com/example/dependencyInjection/controllers/PublicController.java
@@ -1,5 +1,6 @@
 package com.example.dependencyInjection.controllers;
 
+import jakarta.annotation.Nullable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PublicController {
     @RequestMapping("/ping")
-    public String pingMe(@RequestParam String ping) {
-        return ping;
+    public String pingMe(@RequestParam @Nullable String pingBack) {
+        return pingBack != null ? pingBack : "OK";
     }
 }
